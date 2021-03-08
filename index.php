748404a71de0ca9f823055e9fc878093c582ec44
musaeri1807 <musaeri1807@gmail.com>
2021-03-08 16:06:05 +0700

    Updadte

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 $svrname=$_SERVER['SERVER_NAME'];
-// echo $svrname;
-// die();
 
 if ($_SERVER['SERVER_NAME']=='localhost') {
                
   echo '<META HTTP-EQUIV="Refresh" Content="1; URL=https://localhost/Login-Register-PHP-PDO/loginv2">';
 }else{
-  echo '<META HTTP-EQUIV="Refresh" Content="1; URL=https://'.$svrname.'/settingdatabase';
+  header("location:settingdatabase");
 }
 
 ?>
