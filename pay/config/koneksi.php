4b5984ce0b721f86da7e0d61b1c29a3838a8dd65
ANTAM\LM.NetworkAdmin <musaeri1807@gmail.com>
2021-06-07 13:30:53 +0700

    pay

--- pay/config/koneksi.php
+++ pay/config/koneksi.php
@@ -1,9 +1,20 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "dbcrudoop";
+if ($_SERVER['SERVER_NAME']=='localhost') {
+    # code...
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "dbcrudoop";
+} else {
+    # code...
+    $db_host = "localhost";
+    $db_user = "musx1236_musaeri";
+    $db_pass = "P@55.rdmusaeri.123#";
+    $db_name = "musx1236_VPS01NA";
+}
+
+
 
 try {    
     //create PDO connection 
