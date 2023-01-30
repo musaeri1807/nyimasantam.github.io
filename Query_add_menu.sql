3d22470217a30f71c0fbb8c7504522ba0a49cd10
musaeri1807 <musaeri1807@gmail.com>
2023-01-30 13:34:36 +0700

    add Query

---
+++ Query_add_menu.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO tblemployeaccessmenu (field_role_id,field_idmenu) VALUES ('MGR','5');
+SELECT * FROM tblmenusub ORDER BY field_idmenu ASC;
+SELECT * FROM tblemployeaccessmenu ORDER BY field_role_id ASC;
